5dae1bf3679309c00f52ce28f402472f475a9af0
Chris Church <chris.church@gmail.com>
2017-07-04 23:24:40 -0400

    Starting to flesh out configuration

--- guardian/main.go
+++ guardian/main.go
@@ -1,16 +1,55 @@
 package main
 
 import (
+	"fmt"
 	"net"
 
 	"github.com/siddontang/go-mysql/server"
 )
 
+type Behavior int
+
+const (
+	DENY = iota
+	WARN = iota
+	ALL  = iota
+)
+
+type Config struct {
+	DatabasePattern string
+	AllowedSQLFile  string
+	ProxyPort       int
+	DB              struct {
+		User string
+		Pass string
+		Host string
+		Port int
+	}
+	ProxyBehavior Behavior
+}
+
+func loadConfig(configFile string) (Config, error) {
+	return nil, nil
+}
+
 func main() {
 
-	l, _ := net.Listen("tcp", "127.0.0.1:4000")
+	//TODO: parse cmd line args
+
+	config, err := loadConfig(configFile)
+	if err != nil {
+		fmt.Printf("Error loading config file %s: %s", configFile, err.Error())
+	}
+
+	l, _ := net.Listen("tcp", fmt.Sprintf("%s:%s", config.Host, config.Port))
 
 	c, _ := l.Accept()
+
+	proxy := Proxy{
+		conn:       nil,
+		allowedSQL: sqlPattern,
+		behavior:   config.Behavior,
+	}
 
 	// Create a connection with user root and an empty password
 	// We only an empty handler to handle command too
